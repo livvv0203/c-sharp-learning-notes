e59c5541d0d6256d24676daebbe7e71341565f55
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-08-15 22:42:03 -0700

    init repo

---
+++ c-sharp-01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace c_sharp_01
+{
+    class Program
+    {
+        // Entry point 
+        static void Main(string[] args)
+        {
+            // Console class is defined in System
+            Console.WriteLine("This is Jieqing! ");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
