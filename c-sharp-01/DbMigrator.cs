f16f67993a504b6571a8a170dd1c76d98ceb773c
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-08-19 23:42:56 -0700

    Composition - Loosely Coupling

---
+++ c-sharp-01/DbMigrator.cs
@@ -0,0 +1,41 @@
+﻿namespace c_sharp_learning
+{
+    // Composition
+    public class DbMigrator
+    {
+        private readonly Logger _logger;
+
+        // DbMigrator has Logger 
+        public DbMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("Migrating...");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ c-sharp-01/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace c_sharp_learning
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.Write(message);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
