7709c19c979a4baa10e5f67b89ddaff406b6712e
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-08-20 04:07:03 -0700

    Car and Vehicle class

---
+++ c-sharp-01/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace c_sharp_learning
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initialized.");
+        }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
