f9219aa3d0801844338d0de2111a223d21d60807
jieqing <livvv23@bu.edu>
2022-11-19 20:03:11 -0800

    new branch init

--- c-sharp-01/Program.cs
+++ c-sharp-01/Program.cs
@@ -9,16 +9,7 @@ namespace c_sharp_learning
         // Entry point
         public static void Main(string[] args)
         {
-
-            var list = new List<int>();
-            list.Add(1);
-            list.Add(3456);
-            list.Add(23253534);
-
-            int maxNum = SimpleAlgor.findMax(list);
-            
-            Console.WriteLine($"Max Number of List is: {maxNum}");
-            Console.ReadLine();
+            Console.WriteLine("aaa");
         }
     }
 }
