ce2d52a217a5e4a749dffa4a98c7377c11ed213f
jieqing <livvv23@bu.edu>
2022-11-24 14:35:44 -0800

    practice clearance

--- c-sharp-01/Program.cs
+++ c-sharp-01/Program.cs
@@ -1,18 +1,44 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace c_sharp_learning
 {
     class Program
     {
-        // Entry point
+        /// <summary>
+        /// This is the entry point 
+        /// </summary>
         public static void Main(string[] args)
         {
-            Console.WriteLine("aaa");
+            runLoop();
+
+
+
+        }
+
+        public static void runLoop()
+        {
+            string userInput;
+            int counter = 0;
+            do
+            {
+                Console.WriteLine("Please input a number: ");
+                userInput = Console.ReadLine();
+                Console.WriteLine($"User input is: {userInput}, at ----{counter}----");
+                counter++;
+            } while (counter < 3);
         }
     }
 }
+
+
+
+
+
+
 
 
 
